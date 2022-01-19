e0d7bec6d098e435f22db5d56d8894b41f331f4c
Ahmed Eid <ahmed.eid@gmx.com>
2022-01-19 19:40:59 +0300

    modified database configuration file to use cloud foundry provided postgresql instance

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
+const cfenv = require("cfenv");
+const appEnv = cfenv.getAppEnv();
+const service = appEnv.getServices()['postgres-instance'];
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host: service.credentials.hostname,
+      port: service.credentials.port,
+      database: service.credentials.dbname,
+      user: service.credentials.username,
+      password: service.credentials.password,
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
